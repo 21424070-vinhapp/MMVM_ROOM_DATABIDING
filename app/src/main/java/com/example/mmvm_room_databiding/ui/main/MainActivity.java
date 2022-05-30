d0fe4651f0eaad7e281f744fb1ce8537d562a06f
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-30 16:34:30 +0700

    finish insert in room db

--- app/src/main/java/com/example/mmvm_room_databiding/ui/main/MainActivity.java
+++ app/src/main/java/com/example/mmvm_room_databiding/ui/main/MainActivity.java
@@ -17,26 +17,41 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     MainViewModel mainViewModel;
+    private static final String TAG = "BBB";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       mainViewModel=new ViewModelProvider(this, new ViewModelProvider.Factory() {
-           @NonNull
-           @Override
-           public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-               return (T) new MainViewModel(MainActivity.this);
-           }
-       }).get(MainViewModel.class);
+        mainViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new MainViewModel(MainActivity.this);
+            }
+        }).get(MainViewModel.class);
 
-       mainViewModel.getListWork().observe(this, new Observer<List<WorkEntity>>() {
-           @Override
-           public void onChanged(List<WorkEntity> workEntities) {
-               Log.d("BBB", "Total: "+workEntities.size());
-           }
-       });
+        mainViewModel.getListWork().observe(this, new Observer<List<WorkEntity>>() {
+            @Override
+            public void onChanged(List<WorkEntity> workEntities) {
+                Log.d("BBB", "Total: " + workEntities.size());
+            }
+        });
 
-       mainViewModel.queryListWork();
+        mainViewModel.getIdInsert().observe(this, new Observer<Long>() {
+            @Override
+            public void onChanged(Long aLong) {
+                Log.d(TAG, "Row: "+aLong);
+            }
+        });
+
+
+        //get list work in db
+        mainViewModel.queryListWork();
+
+        //insert work in db
+        WorkEntity workEntity = new WorkEntity("to do 1", "nothing 1");
+        mainViewModel.queryInsertWork(workEntity);
     }
 }
