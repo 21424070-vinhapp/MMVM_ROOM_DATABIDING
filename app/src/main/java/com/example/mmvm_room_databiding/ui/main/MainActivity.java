bd47c6248eb8c22dbced01101d0cc83c6c80c482
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-30 13:56:07 +0700

    setup code

--- app/src/main/java/com/example/mmvm_room_databiding/ui/main/MainActivity.java
+++ app/src/main/java/com/example/mmvm_room_databiding/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.mmvm_room_databiding;
+package com.example.mmvm_room_databiding.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.mmvm_room_databiding.R;
 
 public class MainActivity extends AppCompatActivity {
 
